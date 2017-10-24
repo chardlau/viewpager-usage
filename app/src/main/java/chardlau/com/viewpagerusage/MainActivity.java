71f3c4570dda6d91473a28c6a184c70bb5b34e96
liuhuobing <619102150@qq.com>
2017-10-24 10:29:23 +0800

    Bad practice of display dynamic data using ViewPager

--- app/src/main/java/chardlau/com/viewpagerusage/MainActivity.java
+++ app/src/main/java/chardlau/com/viewpagerusage/MainActivity.java
@@ -1,25 +1,33 @@
 package chardlau.com.viewpagerusage;
 
+import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private List<String> testDataSource1 = new ArrayList<>();
+    private DynamicDataSetAdapter adapter;
 
-    private void initialData() {
-        testDataSource1.add("testDataSource1 1");
-        testDataSource1.add("testDataSource1 2");
-        testDataSource1.add("testDataSource1 3");
+    private ViewPager viewPager;
+
+    private List<String> randomData() {
+        List<String> dataSource = new ArrayList<>();
+        long tmp = new Date().getTime();
+        long cnt = tmp % 10 + 1;
+        for (int i = 0; i < cnt; i++) {
+            dataSource.add("DataSource " + (tmp++));
+        }
+        return dataSource;
     }
 
     @Override
@@ -27,22 +35,43 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        initialData();
+        adapter = new DynamicDataSetAdapter();
+        adapter.setTexts(randomData());
 
-//        UpdatePagerAdapter adapter = new UpdatePagerAdapter();
-        Update2PagerAdapter adapter = new Update2PagerAdapter();
-        adapter.setTexts(testDataSource1);
-
-        ViewPager viewPager = (ViewPager) findViewById(R.id.vp_viewpager_update);
+        viewPager = (ViewPager) findViewById(R.id.vp_viewpager_update);
         viewPager.setAdapter(adapter);
+
+        findViewById(R.id.btn_random).setOnClickListener(this);
+        findViewById(R.id.btn_insert).setOnClickListener(this);
+        findViewById(R.id.btn_remove).setOnClickListener(this);
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_random:
+                adapter.setTexts(randomData());
+                viewPager.setAdapter(adapter);
+                break;
 
-    private static class UpdatePagerAdapter extends PagerAdapter {
+            case R.id.btn_insert:
+                adapter.insertText();
+                break;
+
+            case R.id.btn_remove:
+                adapter.removeText();
+                break;
+        }
+    }
+
+    private static class DynamicDataSetAdapter extends PagerAdapter {
 
         private List<String> texts;
 
-        public UpdatePagerAdapter() {
+        private int idGenerator;
+
+        public DynamicDataSetAdapter() {
+            idGenerator = 0;
             texts = new ArrayList<>();
         }
 
@@ -53,7 +82,8 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public boolean isViewFromObject(View view, Object object) {
-            return view.equals(object);
+            String text = (String) view.getTag();
+            return object.equals(text);
         }
 
         @Override
@@ -61,72 +91,42 @@ public class MainActivity extends AppCompatActivity {
             String text = texts.get(position);
 
             TextView textView = new TextView(container.getContext());
+            textView.setTag(text);
             textView.setText(text);
 
             container.addView(textView);
-            return textView;
+            return text;
         }
 
         @Override
         public void destroyItem(ViewGroup container, int position, Object object) {
-            container.removeView((View) object);
+            View view = container.findViewWithTag(object);
+            if (view != null) {
+                container.removeView(view);
+            }
         }
 
-        public void setTexts(List<String> texts) {
+        public synchronized void setTexts(List<String> texts) {
             this.texts.clear();
             if (texts != null && texts.size() > 0) {
                 this.texts.addAll(texts);
             }
             notifyDataSetChanged();
         }
-    }
 
-private static class Update2PagerAdapter extends PagerAdapter {
+        public synchronized void insertText() {
+            idGenerator ++;
+            String text = "Insert_" + idGenerator;
+            this.texts.add(text);
+            notifyDataSetChanged();
+        }
 
-    private List<String> texts;
-
-    public Update2PagerAdapter() {
-        texts = new ArrayList<>();
-    }
-
-    @Override
-    public int getCount() {
-        return texts.size();
-    }
-
-    @Override
-    public boolean isViewFromObject(View view, Object object) {
-        String text = (String) view.getTag();
-        return object.equals(text);
-    }
-
-    @Override
-    public Object instantiateItem(ViewGroup container, int position) {
-        String text = texts.get(position);
-
-        TextView textView = new TextView(container.getContext());
-        textView.setTag(text);
-        textView.setText(text);
-
-        container.addView(textView);
-        return text;
-    }
-
-    @Override
-    public void destroyItem(ViewGroup container, int position, Object object) {
-        View view = container.findViewWithTag(object);
-        if (view != null) {
-            container.removeView(view);
+        public synchronized void removeText() {
+            if (this.texts.size() <= 0) {
+                return;
+            }
+            this.texts.remove(0);
+            notifyDataSetChanged();
         }
     }
-
-    public void setTexts(List<String> texts) {
-        this.texts.clear();
-        if (texts != null && texts.size() > 0) {
-            this.texts.addAll(texts);
-        }
-        notifyDataSetChanged();
-    }
-}
-
 }
